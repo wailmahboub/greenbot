df01684cc3108d7a6a5dd5277ba3eb18c592d4fc
Wail Mahboub <48186938+xXWAILXx@users.noreply.github.com>
2019-03-09 21:32:58 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -278,32 +278,5 @@ client.on("reachLimit", (limit)=> {
 
 
 
-var prefix = "$";
-client.on('message',async message => {/new msg event
-if(!message.channel.guild) return;
- if(message.content.startsWith(prefix + 'Rainbow')) {/to create the rainbow role
-   let role = message.guild.roles.find('name', 'Rainbow Rank')
-   if(role) return message.channel.send(هاي الرتبة موجودة !)/if the role already created return with this msg/start of create role 
- if(!role){
-   rainbow =  message.guild.createRole({
-  name: "Rainbow Rank",/the role will create name
-  color: "#000000",/the default color
-  permissions:[]/the permissions//end of create role
-})
-
-}
-message.channel.send('تم')/if the step completed
-}})
-client.on('ready', () => {/new ready event
- setInterval(function(){
-     client.guilds.forEach(g => {
-                 var role = g.roles.find('name', 'Rainbow Rank');//rainbow role name
-                 if (role) {
-                     role.edit({color : "RANDOM"});
-                 };
-     });
- }, 1000);/the rainbow time
-})
-
 
 client.login(process.env.BOT_TOKEN);
