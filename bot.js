c4647e1757f93f00544310742802d88067c62bb4
xXWAILXx <48186938+xXWAILXx@users.noreply.github.com>
2019-03-09 17:34:27 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -155,7 +155,7 @@ client.on("message", async message => {
             معلومات عن السيرفر : ${prefix}server
             برودكاست للأونلاين فقط : ${prefix}bco
             يعرض لك عدد المتبندين من سيرفرك : ${prefix}banned
-            رابط سيرفر الدعم الفني : dir server dyalk
+            رابط سيرفر الدعم الفني : https://discord.gg/VAdBNFH
             **`);
             message.channel.sendEmbed(help); // رابط السيرفر يعود الى سيرفر CODES .
     }
