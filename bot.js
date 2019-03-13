8a07e73454bcce58da537cca7eab60ed954cc990
Wail Mahboub <48186938+xXWAILXx@users.noreply.github.com>
2019-03-13 10:43:31 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -313,7 +313,37 @@ EpicEdiTeD[message.author.id].Money+= 0.25;
 
 
 
-
+client.on("message",async msg => {
+    if(msg.content.startsWith(prefix + "clear")){
+      let args = msg.content.split(" ").slice(1).join(" ");
+      if(!args)  return msg.reply(`**${msg.content} <Number Messages Deleted?>**`)
+      msg.reply("**Are You Sure Of The Deleted Messages?**").then(o => {
+        o.react("✅")
+        .then(()=> o.react('❎'))
+        .then(()=> o.react("✅"))
+        let reaction1 = (reaction,user) => reaction.emoji.name === "✅" && user.id === msg.author.id
+        let reaction2 = (reaction,user) => reaction.emoji.name === "❎" && user.id === msg.author.id
+        let react3 = o.createReactionCollector(reaction1, { time: 12300})
+        let react4 = o.createReactionCollector(reaction2, { time: 12300})
+        react3.on("collect", r => {
+         msg.channel.bulkDelete(args)
+          msg.reply(`**Done Deleted Messages ${args}**`).then(op => {
+          op.delete(1200)
+         o.delete(1200)
+         msg.delete(1200)
+       })
+       react4.on("collect", r => {
+        msg.reply(`**Done Deleted Messages Has Been Cancel**`).then(ob => {
+          ob.delete(1200)
+          o.delete(1200)
+          msg.delete(1200)
+        })
+        })
+      })
+    })
+    
+    }
+  });
 
 
 client.login(process.env.BOT_TOKEN);
