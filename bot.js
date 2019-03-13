639f2fc1d69f04b1636429e48de41c0687cc7c5c
Wail Mahboub <48186938+xXWAILXx@users.noreply.github.com>
2019-03-13 11:11:51 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -355,6 +355,47 @@ client.on("message",async msg => {
 
 
 
+client.on('message', async message => {
+    if(message.content.includes('discord.gg')){
+        if(message.member.hasPermission("MANAGE_GUILD")) return;
+if(!message.channel.guild) return;
+message.delete()
+  var command = message.content.split(" ")[0];
+let muterole = message.guild.roles.find(`name`, "Muted");
+if(!muterole){
+try{
+muterole = await message.guild.createRole({
+  name: "muted",
+  color: "#000000",
+  permissions:[]
+})
+message.guild.channels.forEach(async (channel, id) => {
+  await channel.overwritePermissions(muterole, {
+    SEND_MESSAGES: false,
+    ADD_REACTIONS: false
+  });
+});
+}catch(e){
+console.log(e.stack);
+}
+}
+   if(!message.channel.guild) return message.reply('** This command only for servers**');
+message.member.addRole(muterole);
+const embed500 = new Discord.RichEmbed()
+.setTitle("ميوت")
+    .addField(`**  تم إعطائك ميوت **` , `**السبب : نشر سيرفرات ديسكورد أخرى**`)
+    .setColor("RANDOM")
+    .setThumbnail(`${message.author.avatarURL}`)
+    .setAuthor(message.author.username, message.author.avatarURL)
+.setFooter(`${message.guild.name} `)
+message.channel.send(embed500)
+message.author.send('` تمت معاقبتك بميوت اذا كا ن ذلك خطأ فيرجى مكالمة الإدارة العليا `');
+ 
+ 
+}
+})//AlphaCodes
+
+
 
 
 
