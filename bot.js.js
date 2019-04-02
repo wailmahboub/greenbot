d89b47eaf96c2dc91a87b10d6eb41c41058e493b
Wail Mahboub <48186938+xXWAILXx@users.noreply.github.com>
2019-04-03 00:05:30 +0100

    Update bot.js.js

--- bot.js.js
+++ bot.js.js
@@ -1303,6 +1303,28 @@ client.on("guildMemberAdd", member => {
 
 
 
+client.on('message',async message => {
+    if(message.content.startsWith("f!restart")) {
+        if(message.author.id !== "ايدي بوتك") return message.reply('You arent the bot owner.');
+        message.channel.send('zZz').then(msg => {
+            setTimeout(() => {
+               msg.edit('zZzZz');
+            },1000);
+            setTimeout(() => {
+               msg.edit('zZzZzZz');
+            },2000);
+        });
+        console.log('Your Bot Has Restarted.');
+        console.log(zZzZz);
+        setTimeout(() => {
+            client.destroy();
+            client.login(process.env.BOT_TOKEN);
+        },3000);
+    }
+});
+
+
+
 
 
 
