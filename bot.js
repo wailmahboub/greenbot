156a1eca63eae38e8106e71d34a29482db5e59e8
Wail Mahboub <48186938+wailmahboub@users.noreply.github.com>
2019-06-01 21:51:25 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -2970,7 +2970,7 @@ client.on("guildMemberAdd", member => {
 
 client.on('ready', function(){
     var ms = 10000 ;
-    var setGame = [' !!help ','Max_Bot | Server ' ];
+    var setGame = [' !!help ','GreenBot ' ];
     var i = -1;
     var j = 0;
     setInterval(function (){
